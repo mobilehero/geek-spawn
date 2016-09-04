7cd9dd915826fe1bc0ea7ea32e6bd9fdd25f87c3
Brenton House <brenton.house@gmail.com>
2016-09-04 16:50:54 -0500

    :checkered_flag: v0.0.1-beta.6 :heavy_minus_sign: adding debug

--- spawn.js
+++ spawn.js
@@ -1,19 +1,28 @@
 'use strict';
 
-var _ = require("lodash");
-var child_process = require('child_process');
-var path = require("path");
+const _ = require("lodash");
+const child_process = require('child_process');
+const path = require("path");
+const chalk = require("chalk");
+const module_name = path.parse(module.id).name;
+const SEPARATOR = process.platform === "win32" ? ";" : ":";
 var env = Object.assign({}, process.env);
-var SEPARATOR = process.platform === "win32" ? ";" : ":";
 env.PATH = path.resolve(path.join(__dirname, "/node_modules/.bin")) + SEPARATOR + env.PATH;
 
-exports.spawnSync = function(cmd, args, opts) {
+// debug logger
+var logger = (func_name) => {
+	var prefix = func_name ? `[${module_name}.${func_name}] ` : `[${module_name}`;
+	return _.wrap(require('debug')('geek-spawn'), (func, msg) => func(chalk.blue(`[${module_name}.execute] `) + msg));
+}
+let debug = logger();
 
+exports.spawnSync = function(cmd, args, opts) {
+	let debug = logger('spawnSync');
 	opts = opts || {};
 	opts.stdio = 'inherit';
 	opts.env = env;
 
-	if (process.platform === 'win32') {
+	if(process.platform === 'win32') {
 		args = ['/c', cmd].concat(args);
 		cmd = process.env.comspec;
 	}
@@ -22,28 +31,30 @@ exports.spawnSync = function(cmd, args, opts) {
 }
 
 exports.spawn = function(cmd, args, opts) {
-
-	var successfulExitCodes = (opts && opts.successfulExitCodes) || [0];
+	let debug = logger('spawn');
 	return new Promise(function(resolve, reject) {
 
 		opts = opts || {};
-		opts.successfulExitCodes = opts.successfulExitCodes || [0];
-		opts.stdio = 'inherit';
-		opts.env = env;
+		_.defaults(opts, {
+			successfulExitCodes: [0],
+			stdio: 'inherit',
+			env: env
+		})
 
-		if (process.platform === 'win32') {
+		if(process.platform === 'win32') {
 			args = ['/c', cmd].concat(args);
 			cmd = process.env.comspec;
 		}
 
+		debug("Spawning in " + opts.cwd + ":  " + cmd + " " + args.join(" "));
 		var child = child_process.spawn(cmd, args, opts);
 
 		child.on('close', (code, signal) => {
 
-			if (!_.includes(opts.successfulExitCodes, code)) {
+			if(!_.includes(opts.successfulExitCodes, code)) {
 				var err = {
 					code: code,
-					message: "'" + cmd + " " + _.join(args," ") + "' failed with code " + code,
+					message: "'" + cmd + " " + _.join(args, " ") + "' failed with code " + code,
 					childProcess: child,
 					toString() {
 						return this.message;
@@ -60,7 +71,7 @@ exports.spawn = function(cmd, args, opts) {
 
 		});
 
-		child.on('error', (err) => {
+		child.on('error', err => {
 			reject(err);
 		});
 
